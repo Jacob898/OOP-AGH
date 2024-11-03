3977d2f2edb7353d82f0b64c810673eb4823f6df
Jacob898 <jstach898@gmail.com>
2024-11-03 13:52:38 +0100

    Added lab3 files

--- oolab/src/main/java/agh/ics/oop/World.java
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -1,34 +1,43 @@
 package agh.ics.oop;
 
+import agh.ics.oop.model.Animal;
 import agh.ics.oop.model.MoveDirection;
-import static agh.ics.oop.OptionsParser.parser;
+import static agh.ics.oop.OptionsParser.parse;
 import agh.ics.oop.model.Vector2d;
 import agh.ics.oop.model.MapDirection;
 
+import java.util.List;
+
 public class World {
     public static void main(String[] args) {
+//        Animal animal = new Animal();
+//        System.out.println(animal.getCoordinates());
 
-        Vector2d position1 = new Vector2d(1,2);
-        System.out.println(position1);
-        Vector2d position2 = new Vector2d(-2,1);
-        System.out.println(position2);
-        System.out.println(position1.add(position2));
+//        Vector2d position1 = new Vector2d(1,2);
+//        System.out.println(position1);
+//        Vector2d position2 = new Vector2d(-2,1);
+//        System.out.println(position2);
+//        System.out.println(position1.add(position2));
+//
+//        MapDirection mapDir = MapDirection.NORTH;
+//        System.out.println(mapDir.toString());
+//        System.out.println(mapDir.next());
+//        System.out.println(mapDir.previous());
+//        System.out.println(mapDir.toUnitVector());
+//
+//        System.out.println("Start");
+//
+//        List<MoveDirection> moveDirections = parser(args);
+//        run(moveDirections);
+//        System.out.println("Stop");
 
-        MapDirection mapDir = MapDirection.NORTH;
-        System.out.println(mapDir.toString());
-        System.out.println(mapDir.next());
-        System.out.println(mapDir.previous());
-        System.out.println(mapDir.toUnitVector());
-
-        System.out.println("Start");
-
-        MoveDirection[] moveDirections = parser(args);
-
-        run(moveDirections);
-        System.out.println("Stop");
+          List<MoveDirection> directions = OptionsParser.parse(args);
+          List<Vector2d> positions = List.of(new Vector2d(2,2), new Vector2d(3,4));
+          Simulation simulation = new Simulation(positions, directions);
+          simulation.run();
     }
 
-    public static void run(MoveDirection[] args){
+    public static void run(List<MoveDirection> args){
 
         for(MoveDirection direction: args){
             switch(direction){
---
+++ oolab/src/test/java/agh/ics/oop/SimulationTest.java
@@ -0,0 +1,71 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.Animal;
+import agh.ics.oop.model.MapDirection;
+import agh.ics.oop.model.MoveDirection;
+import agh.ics.oop.model.Vector2d;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class SimulationTest {
+    @Test
+    public void simulationOfMultipleAnimals() {
+
+        List<MoveDirection> directions = OptionsParser.parse("r l f f 5 g s f f b r b r".split(" "));
+        List<Vector2d> positions = new ArrayList<>();
+
+        positions.add(new Vector2d(2, 2));
+        positions.add(new Vector2d(3, 3));
+
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+
+        List<Animal> animals = simulation.getAnimals();
+
+        assertEquals(2, animals.size());
+        assertTrue(animals.get(0).isAt(new Vector2d(2, 2)));
+        assertTrue(animals.get(1).isAt(new Vector2d(1, 3)));
+
+        for(Animal animal : animals) {
+           assertEquals(MapDirection.EAST,animal.getDirection());
+           assertTrue(animal.getCoordinates().precedes(new Vector2d(4, 4)));
+           assertTrue(animal.getCoordinates().follows(new Vector2d(0,0)));
+       }
+    }
+
+    @Test
+    public void simulationOfSingleAnimal() {
+        List<MoveDirection> directions = OptionsParser.parse("f s f 5 l f".split(" "));
+        List<Vector2d> positions = List.of(new Vector2d(2, 2));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+
+        List<Animal> animals = simulation.getAnimals();
+        assertEquals(1, animals.size());
+        assertTrue(animals.get(0).isAt(new Vector2d(1,4)));
+        assertEquals(MapDirection.WEST,animals.get(0).getDirection());
+        assertTrue(animals.get(0).getCoordinates().follows(new Vector2d(0,0)));
+        assertTrue(animals.get(0).getCoordinates().precedes(new Vector2d(4,4)));
+
+    }
+
+    @Test
+    public void animalDoesntGoOutOfBounds() {
+        List<MoveDirection> directions = OptionsParser.parse("b f".split(" "));
+        List<Vector2d> positions = List.of(new Vector2d(0, 0), new Vector2d(4, 4));
+        Simulation simulation = new Simulation(positions, directions);
+        simulation.run();
+        List<Animal> animals = simulation.getAnimals();
+
+        for (Animal animal : animals) {
+            assertTrue(animal.getCoordinates().follows(new Vector2d(0,0)));
+            assertTrue(animal.getCoordinates().precedes(new Vector2d(4,4)));
+        }
+
+    }
+
+}
